0d374068f3da7e1218c4bec8e8e08c9731c70210
brunomonteiro21@gmail.com <brunomonteiro21@gmail.com@57b4e280-dca3-29b7-585f-ea55c616d593>
2014-08-18 19:40:41 +0000

    Bruno Monteiro -- 18/08/2014 Construindo módulo de produtos, parte [ 2 ].

--- AcademiaJavaAccenture/src/com/impl/repository/ProdutoRepositorio.java
+++ AcademiaJavaAccenture/src/com/impl/repository/ProdutoRepositorio.java
@@ -2,7 +2,9 @@ package com.impl.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.domain.Produto;
@@ -19,9 +21,12 @@ public class ProdutoRepositorio implements InterfaceProduto {
 
 			con.setAutoCommit(false);
 
-			PreparedStatement pStmt = con.prepareStatement(" SQL ");
+			PreparedStatement pStmt = con.prepareStatement("INSERT INTO ");
 
-			// Colocar os atributos
+			pStmt.setString(1, p.getNomeProduto());
+			pStmt.setString(2, p.getDescricaoProduto());
+			pStmt.setString(3, p.getImagemProduto());
+			pStmt.setFloat(4, p.getPrecoProduto());
 
 			pStmt.executeUpdate();
 			con.close();
@@ -33,20 +38,78 @@ public class ProdutoRepositorio implements InterfaceProduto {
 
 	@Override
 	public void alterarProduto(Produto p) {
-		// TODO Auto-generated method stub
+		try {
+			Connection con = Conexao.getConexao();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pStmt = con.prepareStatement("UPDATE");
+
+			pStmt.setString(1, p.getNomeProduto());
+			pStmt.setString(2, p.getDescricaoProduto());
+			pStmt.setString(3, p.getImagemProduto());
+			pStmt.setFloat(4, p.getPrecoProduto());
+
+			pStmt.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			e.getMessage();
+		}
 
 	}
 
 	@Override
 	public void deletarProduto(int idProduto) {
-		// TODO Auto-generated method stub
+		try {
+			Connection con = Conexao.getConexao();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pStmt = con.prepareStatement("DELETE");
+
+			pStmt.setInt(1, idProduto);
+
+			pStmt.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			e.getMessage();
+		}
 
 	}
 
 	@Override
 	public List<Produto> listaDeProduto() {
-		// TODO Auto-generated method stub
+		ResultSet rs;
+		List<Produto> lista = new ArrayList<Produto>();
+		Produto produto = null;
+		
+		try {
+			Connection con = Conexao.getConexao();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pStmt = con.prepareStatement("SELECT * FROM ");
+
+			rs = pStmt.executeQuery();
+			
+			while (rs.next()) {
+				produto = new Produto();
+				
+				produto.setIdProduto(rs.getInt(""));
+				produto.setNomeProduto(rs.getString(""));
+				produto.setDescricaoProduto(rs.getString(""));
+				produto.setImagemProduto(rs.getString(""));
+				produto.setPrecoProduto(rs.getFloat(0));
+				
+				lista.add(produto);
+			}
+			
+			con.close();
+		} catch (SQLException e) {
+			e.getMessage();
+		}
 		return null;
 	}
+	
 
 }
