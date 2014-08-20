c9031b7485e82de7a5b477223f011a3d8a8a35ec
brunomonteiro21@gmail.com <brunomonteiro21@gmail.com@57b4e280-dca3-29b7-585f-ea55c616d593>
2014-08-20 17:08:56 +0000

    Terminei as classes cliente (interface, exceptio, controle), no dia (20/08) Falta ainda a fachada.

---
+++ trunk/AcademiaJavaAccenture/src/com/fachada/FachadaCliente.java
@@ -0,0 +1,15 @@
+package com.fachada;
+
+import com.interfaces.repository.InterfaceCliente;
+
+public class FachadaCliente {
+  
+	private static FachadaCliente instancia;
+	private InterfaceCliente Icliente;
+	
+	public FachadaCliente(){
+		
+		
+	}
+	
+}
