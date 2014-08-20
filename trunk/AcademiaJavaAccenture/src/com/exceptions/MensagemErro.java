b30c1d31d295a8fc006d377ae9d1870eababcf62
brunomonteiro21@gmail.com <brunomonteiro21@gmail.com@57b4e280-dca3-29b7-585f-ea55c616d593>
2014-08-20 15:36:07 +0000

    Bruno Monteiro -- 19/08/2014 Ajustes

--- trunk/AcademiaJavaAccenture/src/com/exceptions/MensagemErro.java
+++ trunk/AcademiaJavaAccenture/src/com/exceptions/MensagemErro.java
@@ -3,9 +3,9 @@ package com.exceptions;
 public enum MensagemErro {
 	
 	//Produto
-	ErroDuplicidade("Produto ja existe na base de dados!"),
-	ErroNaoLocalizado("Produto não existe na base, por favor verificar se o produto pesquisado realmente existe!"),
-	ErroProdutoNulo("Preencha os campos para poder cadastrar o produto!");
+	ERRO_DUPLICIDADE("Produto ja existe na base de dados!"),
+	ERRO_PRODUTO_NAO_LOCALIZAO("Produto não existe na base, por favor verificar se o produto pesquisado realmente existe!"),
+	ERRO_PRODUTO_NULO("Preencha os campos para poder cadastrar o produto!");
 	
 	
 	private final String msg;
