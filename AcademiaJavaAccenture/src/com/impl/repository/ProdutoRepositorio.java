b065bc97b087bf012a87c4039d3e28e75f33d02c
brunomonteiro21@gmail.com <brunomonteiro21@gmail.com@57b4e280-dca3-29b7-585f-ea55c616d593>
2014-08-22 17:59:36 +0000

    Bruno Monteiro -- 22/08/2014 Ajustes

--- AcademiaJavaAccenture/src/com/impl/repository/ProdutoRepositorio.java
+++ AcademiaJavaAccenture/src/com/impl/repository/ProdutoRepositorio.java
@@ -10,58 +10,65 @@ import java.util.List;
 import com.domain.Produto;
 import com.domain.ProdutoCamping;
 import com.domain.ProdutoVestuarioEEngrenagem;
+import com.exceptions.ProdutoComOIdZerado;
 import com.interfaces.repository.InterfaceProduto;
 
 //Bruno Monteiro -- 18/11/2014
 
 public class ProdutoRepositorio implements InterfaceProduto {
 
+	private Connection conexao = null;
+	private PreparedStatement pStmt = null;
+	ResultSet rs = null;
+	
 	@Override
 	public void inserirProduto(Produto produto) {
 		try {
-			Connection con = Conexao.getConexao();
-			con.setAutoCommit(false);
-			PreparedStatement pStmt = null;
+			abrirConexao();
 			
-			//Produto
-			pStmt = con.prepareStatement("INSERT INTO PRODUTO (product_name, product_description, "
+			this.pStmt = this.conexao.prepareStatement("INSERT INTO PRODUTO (product_name, product_description, "
 			+ "product_image, product_price, product_type) VALUE (?,?,?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
 
-			pStmt.setString(1, produto.getNome());
-			pStmt.setString(2, produto.getDescricao());
-			pStmt.setString(3, produto.getImagem());
-			pStmt.setFloat(4, produto.getPreco());
-			pStmt.setString(5, produto.getTipo());
+			this.pStmt.setString(1, produto.getNome());
+			this.pStmt.setString(2, produto.getDescricao());
+			this.pStmt.setString(3, produto.getImagem());
+			this.pStmt.setFloat(4, produto.getPreco());
+			this.pStmt.setString(5, produto.getTipo());
 			
-			pStmt.executeUpdate();
+			this.pStmt.executeUpdate();
 			
 			int chavePrimariaInserida = RepositorioUtil.retornarChavePrimariaInserida(pStmt.getGeneratedKeys());
 
 			//Produto Camping
 			if (produto instanceof ProdutoCamping) {
-				pStmt = con.prepareStatement("INSERT INTO produtocamping " + 
+				this.pStmt = conexao.prepareStatement("INSERT INTO produtocamping " + 
 				"(Product_Id, Product_registraction) VALUE (?,?)");
 
-				pStmt.setInt(1, chavePrimariaInserida);
-				pStmt.setString(2, ((ProdutoCamping) produto).getRegistroDaAgenciaAmbiental());
+				this.pStmt.setInt(1, chavePrimariaInserida);
+				this.pStmt.setString(2, ((ProdutoCamping) produto).getRegistroDaAgenciaAmbiental());
 
-				pStmt.executeUpdate();
+				this.pStmt.executeUpdate();
 			}else if(produto instanceof ProdutoVestuarioEEngrenagem){
-				pStmt = con.prepareStatement("INSERT INTO produtoapparel " + 
+				this.pStmt = conexao.prepareStatement("INSERT INTO produtoapparel " + 
 				"(product_id, product_size, product_color) VALUE (?,?,?)");
 
-				pStmt.setInt(1, chavePrimariaInserida);
-				pStmt.setString(2, ((ProdutoVestuarioEEngrenagem) produto).getTamanho());
-				pStmt.setString(3, ((ProdutoVestuarioEEngrenagem) produto).getCor());
+				this.pStmt.setInt(1, chavePrimariaInserida);
+				this.pStmt.setString(2, ((ProdutoVestuarioEEngrenagem) produto).getTamanho());
+				this.pStmt.setString(3, ((ProdutoVestuarioEEngrenagem) produto).getCor());
 
-				pStmt.executeUpdate();
+				this.pStmt.executeUpdate();
 			}
 
-			con.commit();
-			pStmt.close();
-			con.close();
+			commit();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+		} catch (ProdutoComOIdZerado e1) {
+			rollback();
+			System.out.println(e1.getMessage());
+		} catch (Exception e2){
+			System.out.println(e2.getMessage());
+		} finally{
+			fecharConexao();
 		}
 
 	}
@@ -69,50 +76,51 @@ public class ProdutoRepositorio implements InterfaceProduto {
 	@Override
 	public void alterarProduto(Produto produto) {
 		try {
-			Connection con = Conexao.getConexao();
-			con.setAutoCommit(false);
-			PreparedStatement pStmt = null;
+			abrirConexao();
 			
 			//Produto 
-			pStmt = con.prepareStatement("UPDATE PRODUTO SET product_name = ?, "
+			this.pStmt = this.conexao.prepareStatement("UPDATE PRODUTO SET product_name = ?, "
 			+ "product_description = ?, product_image = ?, product_price = ?, product_type = ? WHERE product_id = ?");
 
-			pStmt.setString(1, produto.getNome());
-			pStmt.setString(2, produto.getDescricao());
-			pStmt.setString(3, produto.getImagem());
-			pStmt.setFloat(4, produto.getPreco());
-			pStmt.setString(5, produto.getTipo());
-			pStmt.setInt(6, produto.getId());
+			this.pStmt.setString(1, produto.getNome());
+			this.pStmt.setString(2, produto.getDescricao());
+			this.pStmt.setString(3, produto.getImagem());
+			this.pStmt.setFloat(4, produto.getPreco());
+			this.pStmt.setString(5, produto.getTipo());
+			this.pStmt.setInt(6, produto.getId());
 
-			pStmt.executeUpdate();
+			this.pStmt.executeUpdate();
 			
 			//Produto camping
 			if(produto instanceof ProdutoCamping){
-				pStmt = con.prepareStatement("UPDATE PRODUTOCAMPING SET "
+				this.pStmt = this.conexao.prepareStatement("UPDATE PRODUTOCAMPING SET "
 				+ "Product_registraction = ? WHERE product_id = ? and Id_produtoCamping = ?");
 
-				pStmt.setString(1, ((ProdutoCamping) produto).getRegistroDaAgenciaAmbiental());
-				pStmt.setInt(2, produto.getId());
-				pStmt.setInt(3, ((ProdutoCamping) produto).getIdCamping());
+				this.pStmt.setString(1, ((ProdutoCamping) produto).getRegistroDaAgenciaAmbiental());
+				this.pStmt.setInt(2, produto.getId());
+				this.pStmt.setInt(3, ((ProdutoCamping) produto).getIdCamping());
 
-				pStmt.executeUpdate();	
+				this.pStmt.executeUpdate();	
 			}else if(produto instanceof ProdutoVestuarioEEngrenagem){
-				pStmt = con.prepareStatement("UPDATE produtoapparel SET "
+				this.pStmt = this.conexao.prepareStatement("UPDATE produtoapparel SET "
 				+ "Product_Size = ?, Product_Color = ? WHERE product_id = ? and Id_produtoApparel = ?");
 
-				pStmt.setString(1, ((ProdutoVestuarioEEngrenagem) produto).getTamanho());
-				pStmt.setString(2, ((ProdutoVestuarioEEngrenagem) produto).getCor());
-				pStmt.setInt(3, produto.getId());
-				pStmt.setInt(4, ((ProdutoVestuarioEEngrenagem) produto).getIdVestuarioEEngrenagem());
+				this.pStmt.setString(1, ((ProdutoVestuarioEEngrenagem) produto).getTamanho());
+				this.pStmt.setString(2, ((ProdutoVestuarioEEngrenagem) produto).getCor());
+				this.pStmt.setInt(3, produto.getId());
+				this.pStmt.setInt(4, ((ProdutoVestuarioEEngrenagem) produto).getIdVestuarioEEngrenagem());
 
-				pStmt.executeUpdate();
+				this.pStmt.executeUpdate();
 			}
 			
-			con.commit();
-			pStmt.close();
-			con.close();
+			commit();
+			
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
+		} finally {
+			fecharConexao();
 		}
 
 	}
@@ -120,190 +128,182 @@ public class ProdutoRepositorio implements InterfaceProduto {
 	@Override
 	public void deletarProduto(Produto produto) {
 		try {
-			Connection con = Conexao.getConexao();
-			con.setAutoCommit(false);
-			PreparedStatement pStmt = null;
+			abrirConexao();
 			
 			//Produto Camping
 			if(produto instanceof ProdutoCamping){
-				pStmt = con.prepareStatement("DELETE FROM produtocamping WHERE product_id = ?");
+				this.pStmt = this.conexao.prepareStatement("DELETE FROM produtocamping WHERE product_id = ?");
 
-				pStmt.setInt(1, produto.getId());
+				this.pStmt.setInt(1, produto.getId());
 				
-				pStmt.executeUpdate();
+				this.pStmt.executeUpdate();
 			}else if (produto instanceof ProdutoVestuarioEEngrenagem){
-				pStmt = con.prepareStatement("DELETE FROM produtoapparel WHERE product_id = ?");
+				this.pStmt = this.conexao.prepareStatement("DELETE FROM produtoapparel WHERE product_id = ?");
 
-				pStmt.setInt(1, produto.getId());
+				this.pStmt.setInt(1, produto.getId());
 				
-				pStmt.executeUpdate();	
+				this.pStmt.executeUpdate();	
 			}
 			
 			//Produto
-			pStmt = con.prepareStatement("DELETE FROM PRODUTO WHERE product_id = ?");
+			this.pStmt = conexao.prepareStatement("DELETE FROM PRODUTO WHERE product_id = ?");
 			
-			pStmt.setInt(1, produto.getId());
+			this.pStmt.setInt(1, produto.getId());
 			
-			pStmt.executeUpdate();
+			this.pStmt.executeUpdate();
 			
-			con.commit();
-			pStmt.close();
-			con.close();
+			commit();
 		} catch (SQLException e) {
-			e.getMessage();
+			System.out.println(e.getMessage());
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
+		} finally{
+			fecharConexao();
 		}
 
 	}
 
 	@Override
 	public List<Produto> listaDeProduto() {
-		ResultSet rs;
+		
 		List<Produto> lista = new ArrayList<Produto>();
 		Produto produto = null;
 
 		try {
-			Connection con = Conexao.getConexao();
+			abrirConexao();
 
-			con.setAutoCommit(false);
+			this.pStmt = this.conexao.prepareStatement("SELECT * FROM PRODUTO");
 
-			PreparedStatement pStmt = con.prepareStatement("SELECT * FROM PRODUTO");
+			this.rs = this.pStmt.executeQuery();
 
-			rs = pStmt.executeQuery();
-
-			while (rs.next()) {
-				produto = camposEmComum(rs);
+			while (this.rs.next()) {
+				produto = camposEmComum(this.rs);
 				lista.add(produto);
 			}
-
-			pStmt.close();
-			con.close();
+			
 		} catch (SQLException e) {
-			e.getMessage();
+			System.out.println(e.getMessage());
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
+		} finally{
+			fecharConexao();
 		}
 		return lista;
 	}
 
 	@Override
 	public List<ProdutoCamping> listaDeProdutoCamping() {
-		ResultSet rs;
 		List<ProdutoCamping> lista = new ArrayList<ProdutoCamping>();
 		ProdutoCamping produtoCamping = null;
 
 		try {
-			Connection con = Conexao.getConexao();
+			abrirConexao();
 
-			con.setAutoCommit(false);
-
-			PreparedStatement pStmt = con.prepareStatement("select * from produto right join " + 
+			this.pStmt = this.conexao.prepareStatement("select * from produto right join " + 
 			"produtocamping on produto.Product_Id = produtocamping.Product_Id");
 
-			rs = pStmt.executeQuery();
+			this.rs = this.pStmt.executeQuery();
 
-			while (rs.next()) {
-				produtoCamping = (ProdutoCamping) camposEmComum(rs);
+			while (this.rs.next()) {
+				produtoCamping = (ProdutoCamping) camposEmComum(this.rs);
 				
-				produtoCamping.setIdCamping(rs.getInt("Id_produtoCamping"));
-				produtoCamping.setRegistroDaAgenciaAmbiental(rs.getString("Product_registraction"));
+				produtoCamping.setIdCamping(this.rs.getInt("Id_produtoCamping"));
+				produtoCamping.setRegistroDaAgenciaAmbiental(this.rs.getString("Product_registraction"));
 
 				lista.add(produtoCamping);
 			}
-
-			pStmt.close();
-			con.close();
 		} catch (SQLException e) {
-			e.getMessage();
+			System.out.print(e.getMessage());
+		} catch (Exception e1){
+			System.out.println(e1.getMessage());
+		} finally{
+			fecharConexao();
 		}
 		return lista;
 	}
 
 	@Override
 	public List<ProdutoVestuarioEEngrenagem> listaDeProdutoVestuarioEEngrenagem() {
-		ResultSet rs;
 		List<ProdutoVestuarioEEngrenagem> lista = new ArrayList<ProdutoVestuarioEEngrenagem>();
 		ProdutoVestuarioEEngrenagem produtoVestuarioEEngrenagem = null;
 
 		try {
-			Connection con = Conexao.getConexao();
+			abrirConexao();
 
-			con.setAutoCommit(false);
-
-			PreparedStatement pStmt = con.prepareStatement("select * from produto right join " + 
+			this.pStmt = this.conexao.prepareStatement("select * from produto right join " + 
 			"produtoapparel on produto.Product_Id = produtoapparel.Product_Id");
 
-			rs = pStmt.executeQuery();
+			this.rs = this.pStmt.executeQuery();
 
-			while (rs.next()) {
-				produtoVestuarioEEngrenagem = (ProdutoVestuarioEEngrenagem) camposEmComum(rs);
+			while (this.rs.next()) {
+				produtoVestuarioEEngrenagem = (ProdutoVestuarioEEngrenagem) camposEmComum(this.rs);
 				
-				produtoVestuarioEEngrenagem.setIdVestuarioEEngrenagem(rs.getInt("Id_produtoApparel"));
-				produtoVestuarioEEngrenagem.setCor(rs.getString("Product_Color"));
-				produtoVestuarioEEngrenagem.setTamanho(rs.getString("Product_Size"));
+				produtoVestuarioEEngrenagem.setIdVestuarioEEngrenagem(this.rs.getInt("Id_produtoApparel"));
+				produtoVestuarioEEngrenagem.setCor(this.rs.getString("Product_Color"));
+				produtoVestuarioEEngrenagem.setTamanho(this.rs.getString("Product_Size"));
 
 				lista.add(produtoVestuarioEEngrenagem);
 			}
-
-			pStmt.close();
-			con.close();
 		} catch (SQLException e) {
-			e.getMessage();
+			System.out.println(e.getMessage());
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
 		}
 		return lista;
 	}
 
 	@Override
 	public Produto pesquisarProdutoPeloNome(String nome) {
-		ResultSet rs;
 		Produto produto = null;
 
 		try {
-			Connection con = Conexao.getConexao();
+			abrirConexao();
 
-			con.setAutoCommit(false);
-
-			PreparedStatement pStmt = con.prepareStatement("SELECT * FROM PRODUTO WHERE Product_Name like ?");
+			this.pStmt = this.conexao.prepareStatement("SELECT * FROM PRODUTO WHERE Product_Name like ?");
 			
-			pStmt.setString(1, nome);
+			this.pStmt.setString(1, nome);
 
-			rs = pStmt.executeQuery();
+			this.rs = this.pStmt.executeQuery();
 
-			while (rs.next()) {
-				produto = camposEmComum(rs);
+			while (this.rs.next()) {
+				produto = camposEmComum(this.rs);
 			}
 
-			pStmt.close();
-			con.close();
 		} catch (SQLException e) {
 			e.getMessage();
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
+		} finally{
+			fecharConexao();
 		}
 		return produto;
 	}
 
 	@Override
 	public List<Produto> pesquisarProdutoPorTipo(String tipo) {
-		ResultSet rs;
 		Produto produto = null;
 		List<Produto> lista = new ArrayList<Produto>();
 		try {
-			Connection con = Conexao.getConexao();
+			abrirConexao(); 
 
-			con.setAutoCommit(false);
-
-			PreparedStatement pStmt = con.prepareStatement("SELECT * FROM PRODUTO WHERE Product_type like ?");
+			this.pStmt = this.conexao.prepareStatement("SELECT * FROM PRODUTO WHERE Product_type like ?");
 			
-			pStmt.setString(1, tipo);
+			this.pStmt.setString(1, tipo);
 
-			rs = pStmt.executeQuery();
+			this.rs = this.pStmt.executeQuery();
 
-			while (rs.next()) {
-				produto = camposEmComum(rs);
+			while (this.rs.next()) {
+				produto = camposEmComum(this.rs);
 				
 				lista.add(produto);
 			}
 
-			pStmt.close();
-			con.close();
 		} catch (SQLException e) {
-			e.getMessage();
+			System.out.println(e.getMessage());
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
+		} finally{
+			fecharConexao();
 		}
 		return lista;
 	}
@@ -321,4 +321,32 @@ public class ProdutoRepositorio implements InterfaceProduto {
 		return produto;
 	}
 
+	public void abrirConexao() throws SQLException{
+		this.conexao = Conexao.getConexao();
+		this.conexao.setAutoCommit(false);
+	}
+	
+	public void commit() throws SQLException{
+		this.conexao.commit();
+	}
+	
+	public void fecharConexao(){
+		try {
+			this.pStmt.close();
+			this.conexao.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void rollback(){
+		try {
+			conexao.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
